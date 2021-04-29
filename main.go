954b7a30d2070837e65d576773ef24b886b912b6
Tamal Saha <tamal@appscode.com>
2021-04-29 12:55:03 -0700

    added-all

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"k8s.io/client-go/transport"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -15,6 +16,46 @@ import (
 	"k8s.io/client-go/util/homedir"
 )
 
+type enableResponseCaching struct {
+	rt http.RoundTripper
+}
+
+func (a *enableResponseCaching) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := a.rt.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	resp.Header.Set("Cache-Control", "max-age=300") // cache response for 5 minutes
+	return resp, nil
+}
+
+func EnableResponseCaching(rt http.RoundTripper) http.RoundTripper {
+	return &enableResponseCaching{rt}
+}
+
+var _ http.RoundTripper = &enableResponseCaching{}
+
+type acceptCachedResponse struct {
+	rt http.RoundTripper
+}
+
+func (u *acceptCachedResponse) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Cache-Control", "only-if-cached")
+	return u.rt.RoundTrip(req)
+}
+
+func AcceptCachedResponse(rt http.RoundTripper) http.RoundTripper {
+	return &acceptCachedResponse{rt}
+}
+
+var _ http.RoundTripper = &acceptCachedResponse{}
+
+func CacheResponse(rt http.RoundTripper) http.RoundTripper {
+	t := httpcache.NewMemoryCacheTransport()
+	t.Transport = rt
+	return t
+}
+
 func main() {
 	masterURL := ""
 	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
@@ -24,19 +65,30 @@ func main() {
 		log.Fatalf("Could not get Kubernetes config: %s", err)
 	}
 
-	config.Wrap(func(rt http.RoundTripper) http.RoundTripper {
-		t := httpcache.NewMemoryCacheTransport()
-		t.Transport = rt
-		return t
-	})
+	config.Wrap(transport.Wrappers(EnableResponseCaching, CacheResponse))
+
+	//config.Wrap(func(rt http.RoundTripper) http.RoundTripper {
+	//	t := httpcache.NewMemoryCacheTransport()
+	//
+	//	t.Transport = &enableResponseCaching{rt}
+	//	return t
+	//})
 
 	dc := dynamic.NewForConfigOrDie(config)
 
-	nodes, err := dc.Resource(schema.GroupVersionResource{
+	gvrNode := schema.GroupVersionResource{
 		Group:    "",
 		Version:  "v1",
 		Resource: "nodes",
-	}).List(context.TODO(), metav1.ListOptions{})
+	}
+	nodes, err := dc.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, obj := range nodes.Items {
+		fmt.Printf("%+v\n", obj.GetName())
+	}
+	nodes, err = dc.Resource(gvrNode).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		panic(err)
 	}
@@ -56,7 +108,7 @@ func main() {
 		Version:  "v1",
 		Resource: "pods",
 	}
-	result, err := dc.Resource(gvrPod).Namespace("default").List(context.TODO(), metav1.ListOptions{
+	result, err := dc.Resource(gvrPod).Namespace("kube-system").List(context.TODO(), metav1.ListOptions{
 		LabelSelector: sel.String(),
 	})
 	if err != nil {
